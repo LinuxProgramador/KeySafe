159a1708499f9e7a4eeeeb422d45e187db1a373e
LinuxProgramador <154765747+LinuxProgramador@users.noreply.github.com>
2024-07-06 14:03:09 -0600

    sv.py

--- sv.py
+++ sv.py
@@ -187,6 +187,9 @@ Help Menu:
         except PermissionError as p:
             print(f"Permissions error on the file or directory => {p}")
 
+        except (ModuleNotFoundError,ImportError):
+            print("Module or function not imported")
+
         except:
             print("Invalid or corrupt password!")
 
