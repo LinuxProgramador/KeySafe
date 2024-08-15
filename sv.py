20fee9a45aff997da2431fb50a7da59e70e20c4b
LinuxProgramador <154765747+LinuxProgramador@users.noreply.github.com>
2024-08-15 12:38:21 -0600

    sv.py

--- sv.py
+++ sv.py
@@ -19,7 +19,7 @@ class SecureVault:
         self.symbols_and_numbers = ["@", "1", "/", "*", "8", "_", "6", "0", "'", "2", '"', "\\", "+", "9", "&", "3", "-", ";", "4", "!", "?", "5", "#", "$", "7"]
         self.alpha = list("abcdefghijklmnopqrstuvwxyz")
         self.characters = self.symbols_and_numbers + self.alpha
-        self.key_length = choice(range(15 ,32))
+        self.key_length = choice(range(15 ,33))
         self.fernet_key = Fernet.generate_key()
         self.user = getuser()
         self.version_info = "SecureVault 1.0. It is a tool that allows you to generate secure keys."
