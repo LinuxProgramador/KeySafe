a7f73b4772198bbd3dc1e50d1039950f64f63943
LinuxProgramador <154765747+LinuxProgramador@users.noreply.github.com>
2024-09-16 14:25:04 -0600

    sv.py

--- sv.py
+++ sv.py
@@ -37,7 +37,7 @@ class SecureVault:
        '''
        Function that allows me to catch the signal produced by the ctrl_z key.
        '''
-       print("Operation not permitted!")
+       print("\nOperation not permitted!")
        exit(1)
         
         
