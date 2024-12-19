a10c95b9943844a24128f901253eefd5d9368c24
JP Rojas <154765747+LinuxProgramador@users.noreply.github.com>
2024-12-18 20:08:36 -0600

    sv.py

--- sv.py
+++ sv.py
@@ -461,7 +461,7 @@ Help Menu:
 if __name__ == "__main__":
    try:
     #Gets the user who owns the sv.py file. 
-    owner = getpwuid(stat("sv.py").st_uid).pw_name
+    owner = getpwuid(stat(f"/home/{getuser()}/KeySafe/sv.py").st_uid).pw_name
     #Check that the script is not suspended for security reasons, (on some distros it may not work as expected, but this is unlikely)  
     process = run(['/usr/bin/ps', 'aux'], text=True, check=True, capture_output=True)
     output = [line for line in process.stdout.splitlines() if 'sv.py' in line]
