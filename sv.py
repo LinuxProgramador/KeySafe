9bc3802768d40a653cdd9135c806ad3063a8254a
JP Rojas <154765747+LinuxProgramador@users.noreply.github.com>
2024-12-22 18:52:54 -0600

    sv.py

--- sv.py
+++ sv.py
@@ -2,6 +2,25 @@
 
 #Tool to generate secure keys and store them safely on Linux distros
 
+from signal import signal, SIGTSTP, SIGINT
+
+def handle_tstp_signal(signum,frame):
+    '''   
+    Function that allows me to catch the signal produced by the ctrl_z key.
+    '''
+    print("\nOperation not permitted!")
+    exit(1)
+
+def handle_int_signal(signum,frame):
+    '''
+    Function that allows me to catch the signal produced by the ctrl_c key.
+    '''
+    print("\nOperation canceled by user!")
+    exit(1)
+
+signal(SIGINT, handle_int_signal)
+signal(SIGTSTP, handle_tstp_signal)
+
 from secrets import choice
 from sys import argv, exit
 from os import chmod, path, mkdir, remove, listdir, stat, urandom
@@ -10,7 +29,6 @@ from bcrypt import checkpw, hashpw, gensalt
 from getpass import getpass, getuser
 from string import ascii_lowercase, digits, ascii_uppercase
 from subprocess import run, CalledProcessError
-from signal import signal, SIGTSTP
 from shutil import copy
 from datetime import datetime
 from pwd import getpwuid
@@ -530,8 +548,7 @@ if __name__ == "__main__":
       print(f"Path or file does not exist => {e}")
    except (KeyError,ValueError,LookupError):
       print("Error getting owner of file sv.py!")
-   except (KeyboardInterrupt,EOFError):
-      print("\nOperation canceled by user!")
+   
        
 __name__="SecureVault"
 __version__="1.0"
